2503a7087e14c37d0e84de4b06385abc15900322
akane222 <akane222@users.noreply.github.com>
2020-03-18 12:20:09 +0900

    自動更新機能の実装

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,9 @@
 $(function(){ 
+  
   function buildHTML(message){
-    if ( message.image ) {
+    if ( message.content && message.image ) {
       var html =
-        `<div class="chat-main__message-list__message">
+        `<div class="chat-main__message-list__message" data-message-id= ${message.id}>
           <div class="chat-main__message-list__message__info">
             <div class="chat-main__message-list__message__info__talker">
               ${message.user_name}
@@ -19,9 +20,9 @@ $(function(){
           </div>
         </div>`
       return html;
-    } else {
+    } else  if ( message.content ) {
       var html =
-        `<div class="chat-main__message-list__message">
+        `<div class="chat-main__message-list__message" data-message-id= ${message.id}>
           <div class="chat-main__message-list__message__info">
             <div class="chat-main__message-list__message__info__talker">
               ${message.user_name}
@@ -37,7 +38,23 @@ $(function(){
           </div>
         </div>`
       return html;
-    };
+    } else if ( message.image ) {
+      var html =
+      `<div class="chat-main__message-list__message" data-message-id= ${message.id}>
+        <div class="chat-main__message-list__message__info">
+          <div class="chat-main__message-list__message__info__talker">
+            ${message.user_name}
+          </div>
+          <div class="chat-main__message-list__message__info__date">
+            ${message.created_at}
+          </div>
+        </div>
+        <div class="lower-message"> 
+          <img src=${message.image}>
+        </div>
+      </div>`
+    return html;
+    }
   }
   $('#new_message').on('submit', function(e){
     e.preventDefault();
@@ -55,11 +72,40 @@ $(function(){
        var html = buildHTML(data);
        $('.chat-main__message-list').append(html);      
        $('form')[0].reset();
-       $('.submit-btn').prop('disabled', false);
        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
      })
+
+     .always(function(){
+      $('.submit-btn').prop('disabled', false);
+     })
+
      .fail(function(){
       alert('メッセージを入力してください');
     });
   })
+  var reloadMessages = function() {
+    var last_message_id = $('.chat-main__message-list__message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+       var insertHTML = '';
+       $.each(messages, function(i, message) {
+         insertHTML += buildHTML(message)
+       });
+       $('.chat-main__message-list').append(insertHTML);
+       $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+  if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+    setInterval(reloadMessages, 1000);
+  }
 });
