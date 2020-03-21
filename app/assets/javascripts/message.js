f84ecc554d73f7de2969caf3b28ac9fdcd878039
akane222 <akane222@users.noreply.github.com>
2020-03-21 14:18:10 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -70,7 +70,7 @@ $(function(){
     })
      .done(function(data){
        var html = buildHTML(data);
-       $('.chat-main__message-list').append(html);      
+       $('.chat-main__message-list').append(html);
        $('form')[0].reset();
        $('.chat-main__message-list').animate({ scrollTop: $('.chat-main__message-list')[0].scrollHeight});
      })
